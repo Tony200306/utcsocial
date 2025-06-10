e962ea3f2cb4741af63e589cbef37cf227ccdb4b
Admin <tutquan456@gmail.com>
2025-06-10 08:21:09 +0700

    sdsdsd

--- lib/userApi.ts
+++ lib/userApi.ts
@@ -8,7 +8,7 @@ const getContentType = (data: any) => {
 };
 
 const axiosClient = axios.create({
-  baseURL: "https://6253-42-118-89-152.ngrok-free.app/api",
+  baseURL: "https://97f4-42-112-156-206.ngrok-free.app/api",
   // baseURL: "http://localhost:5000/api",
 
   withCredentials: true,
